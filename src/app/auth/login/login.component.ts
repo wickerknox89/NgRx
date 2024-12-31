aed56ed317966c463b7e5565076a1645cd01f18e
wickerknox <wickerknox89@gmail.com>
2024-12-30 20:48:02 -0800

    Actions and Reducers : The Store Service API - Implementing the Login Screen

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,6 +7,7 @@ import {AuthService} from "../auth.service";
 import {tap} from "rxjs/operators";
 import {noop} from "rxjs";
 import {Router} from "@angular/router";
+import { AppState } from '../reducers';
 
 @Component({
   selector: 'login',
@@ -20,7 +21,8 @@ export class LoginComponent implements OnInit {
   constructor(
       private fb:FormBuilder,
       private auth: AuthService,
-      private router:Router) {
+      private router:Router,
+      private store: Store<AppState>) {
 
       this.form = fb.group({
           email: ['test@angular-university.io', [Validators.required]],
@@ -34,7 +36,22 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    const val = this.form.value;
 
+      this.auth.login(val.email, val.password)
+          .pipe(
+              tap(user => {
+                  console.log(user);
+
+                  // this.store.dispatch(login({user}));
+                  
+                  this.router.navigateByUrl('/courses');
+              })
+          )
+          .subscribe(
+              noop,
+              () => alert('Login Failed')
+          );
   }
 
 }
