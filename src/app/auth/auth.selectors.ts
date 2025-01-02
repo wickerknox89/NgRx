857e5d0620738f31aa880043dc1ed24a4eeb3dbb
wickerknox <wickerknox89@gmail.com>
2025-01-02 10:33:04 -0800

    Selectors and Effects : NgRx FeatureSelectors

--- src/app/auth/auth.selectors.ts
+++ src/app/auth/auth.selectors.ts
@@ -1,8 +1,11 @@
-import { createSelector } from "@ngrx/store";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { AuthState } from "./reducers";
+
+export const selectAuthState = createFeatureSelector<AuthState>("auth");
 
 export const isLoggedIn = createSelector(
-    state => state["auth"],
-    (auth) =>  !!auth.user
+    selectAuthState,
+    auth => !!auth.user
 );
 
 export const isLoggedOut = createSelector(
