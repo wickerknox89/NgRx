8931de52180f068f5f55ecc8f0df7cd319c0d16b
wickerknox <wickerknox89@gmail.com>
2025-01-02 11:14:24 -0800

    Selectors and Effects : Implementing User Logout

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -22,11 +22,18 @@ export const initialAuthState : AuthState = {
 };
 
 export const authReducer = createReducer(
-  initialAuthState, 
+  initialAuthState,
+
   on(AuthActions.login, (state, action) => {
   return{
     user: action.user
   }
-})
+}),
+
+  on(AuthActions.logout, (state, action) => {
+    return{
+      user: undefined
+    }
+  })
 );
 
