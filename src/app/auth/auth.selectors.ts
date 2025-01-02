2bb0a96230e79c3b6f2ba991c0721e10559425d3
wickerknox <wickerknox89@gmail.com>
2025-01-02 10:04:50 -0800

    Selectors and Effects : NgRx Selectors - isLoggedIn, isLoggedOut

---
+++ src/app/auth/auth.selectors.ts
@@ -0,0 +1,12 @@
+import { createSelector } from "@ngrx/store";
+
+export const isLoggedIn = createSelector(
+    state => state["auth"],
+    (auth) =>  !!auth.user
+);
+
+export const isLoggedOut = createSelector(
+    isLoggedIn,
+    loggedIn => !loggedIn
+);
+
