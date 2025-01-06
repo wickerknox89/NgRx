f4ae6ad439a488f3869823d1b38029f661d15efb
wickerknox <wickerknox89@gmail.com>
2025-01-06 13:11:21 -0800

    Separating reducers for 'auth' and 'app'

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -5,46 +5,16 @@ import {
   createFeatureSelector,
   createReducer,
   createSelector,
-  MetaReducer,
-  on
+  MetaReducer
 } from '@ngrx/store';
-import { User } from '../auth/model/user.model';
-import { AuthActions } from '../auth/action-types';
 import { routerReducer } from '@ngrx/router-store';
-
-export const authFeatureKey = 'auth';
-
-export interface AuthState {
-  user: User
-}
 
 export interface AppState {
 
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-  router: routerReducer
+    router: routerReducer
 };
 
-
-
-export const initialAuthState : AuthState = {
-  user : undefined
-};
-
-export const authReducer = createReducer(
-  initialAuthState,
-
-  on(AuthActions.login, (state, action) => {
-  return{
-    user: action.user
-  }
-}),
-
-  on(AuthActions.logout, (state, action) => {
-    return{
-      user: undefined
-    }
-  })
-);
 
