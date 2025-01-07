1e99f66c3104f4e2db2f0ee63026454c63308e8e
wickerknox <wickerknox89@gmail.com>
2025-01-06 17:49:48 -0800

    NgRx-DevTools : NgRx - MetaReducers implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import {EffectsModule} from '@ngrx/effects';
 import {EntityDataModule} from '@ngrx/data';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AuthGuard } from './auth/auth.guard';
-import { reducers } from './reducers';
+import { metaReducers, reducers } from './reducers';
 
 
 const routes: Routes = [
@@ -53,6 +53,7 @@ const routes: Routes = [
         MatToolbarModule,
         AuthModule.forRoot(),
         StoreModule.forRoot(reducers, {
+          metaReducers,
           runtimeChecks: {
             strictStateImmutability: true,
             strictActionImmutability: true,
