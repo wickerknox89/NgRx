18a696b3510bccf825b43ebbc48d3e86ae8f9a3b
wickerknox <wickerknox89@gmail.com>
2025-01-06 17:49:48 -0800

    NgRx-DevTools : NgRx - MetaReducers implementation

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -8,6 +8,7 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { routerReducer } from '@ngrx/router-store';
+import { environment } from '../../environments/environment';
 
 export interface AppState {
 
@@ -16,5 +17,18 @@ export interface AppState {
 export const reducers: ActionReducerMap<AppState> = {
     router: routerReducer 
 };
+
+export function logger(reducer:ActionReducer<any>) : ActionReducer<any> {
+    return (state, action) => {
+        console.log("state before: ", state);
+        console.log("action", action);
+
+        return reducer(state, action);
+    }
+
+}
+
+export const metaReducers: MetaReducer<AppState>[] =
+    !environment.production ? [logger] : [];
 
 
