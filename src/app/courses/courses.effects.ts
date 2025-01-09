b5bc31b10403621ee73aa91fb524d74c38dda112
wickerknox <wickerknox89@gmail.com>
2025-01-09 14:17:42 -0800

    NgRx Entity : Optimistic Data Editing - Saving Data in the background with an Effect

--- src/app/courses/courses.effects.ts
+++ src/app/courses/courses.effects.ts
@@ -21,6 +21,18 @@ export class CoursesEffects{
                     )
     );
 
+    saveCourse$ = createEffect(
+        () => this.actions$
+            .pipe(
+                ofType(CourseActions.courseUpdated),
+                concatMap(action => this.coursesHttpService.saveCourse(
+                    action.update.id,
+                    action.update.changes
+                ))
+            ),
+        {dispatch: false}
+    );
+
 }
 
 
